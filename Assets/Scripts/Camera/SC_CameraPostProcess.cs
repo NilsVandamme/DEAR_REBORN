658ddc5bc68f5377dd1300873a427cb11a7b8ca8
Nils Vandamme <nils.vandamme98@gmail.com>
2020-01-19 13:27:47 +0100

    camera prefab + postprocess + menu

---
+++ Assets/Scripts/Camera/SC_CameraPostProcess.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SC_CameraPostProcess : MonoBehaviour
+{
+    public PostProcessVolume[] pplist;
+    public bool retroMode;
+    public Toggle tog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pplist = GetComponents<PostProcessVolume>();
+
+        if (PlayerPrefs.HasKey("retroMode"))
+        {
+            Debug.Log("key");
+            int intvalue = PlayerPrefs.GetInt("retroMode");
+
+            if (intvalue == 0)
+            {
+                retroMode = false;
+                if (SceneManager.GetActiveScene().name == "L_00Menu")
+                    tog.isOn = false;
+            }
+            else
+            {
+                retroMode = true;
+                if (SceneManager.GetActiveScene().name == "L_00Menu")
+                    tog.isOn = true;
+            }
+        }
+        else
+        {
+            Debug.Log("nokey");
+            PlayerPrefs.SetInt("retroMode", 0);
+            retroMode = false;
+            tog.isOn = false;
+        }
+    }
+
+    public void retroModeChange(bool value)
+    {
+        pplist[0].enabled = value;
+
+        if (value)
+            PlayerPrefs.SetInt("retroMode", 1);
+        else
+            PlayerPrefs.SetInt("retroMode", 0);
+    }
+}
