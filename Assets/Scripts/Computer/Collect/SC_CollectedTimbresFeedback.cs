6aab415e26155d04fc1ac8868a343f47b871a864
julien <j.crepin@rubika-edu.com>
2020-01-27 09:39:09 +0100

    modif

--- Assets/Scripts/Computer/Collect/SC_CollectedTimbresFeedback.cs
+++ Assets/Scripts/Computer/Collect/SC_CollectedTimbresFeedback.cs
@@ -74,7 +74,7 @@ public class SC_CollectedTimbresFeedback : MonoBehaviour
         //transform.GetChild(1).gameObject.SetActive(false);
         timer = 0;
         Moving = false;
-
+        Debug.Log("aaaaaaahhhhhhhhhhhh");
         SC_GM_Timbre.gm.Affiche(SC_GM_Master.gm.timbres.timbres[2]);
         anim.Play("FbTimbreIdle");
     }
