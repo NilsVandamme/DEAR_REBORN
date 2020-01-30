b9c41cfbeafe21c43547432b7123e89c64ab0334
julien <j.crepin@rubika-edu.com>
2020-01-30 14:41:48 +0100

    Refonte fichier

---
+++ Assets/Editor/Paragraphe/SC_AutoCompleteEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SC_AutoComplete))]
+public class SC_AutoCompleteEditor : Editor
+{
+    private SC_AutoComplete autocomplete;
+    SC_Base baseInfo;
+
+    private void OnEnable()
+    {
+        autocomplete = target as SC_AutoComplete;
+        baseInfo = (SC_Base)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("Base", new[] { "Assets/ScriptableObjects" })[0]), typeof(SC_Base));
+    }
+
+    /*
+     * Display l'inspector de l'asset créer
+     */
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.LabelField("Champs Lexical");
+        autocomplete.grammarCritere = EditorGUILayout.Popup(autocomplete.grammarCritere, baseInfo.listOfGrammarCritere);
+
+        EditorGUILayout.EndHorizontal();
+
+        base.OnInspectorGUI();
+        EditorUtility.SetDirty(autocomplete);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Computer/Collect/SC_Collect.cs
@@ -0,0 +1,70 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Manage the collect panel on the computer
+
+public class SC_Collect : MonoBehaviour
+{
+    public GameObject diode;
+    public Sprite diodeON;
+    public Sprite diodeOFF;
+    public Button goToPreparatoryPhase;
+
+    public GameObject imageCL; // Button openning or closing the panel
+
+    private Image[] imagesDiodes;
+    private Image[] imagesCL; // All buttons showing collected CLs
+    private TextMeshProUGUI[] listOfTextCL; // Text of the buttons showing CLs
+
+    public static SC_Collect instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        // Get the lists
+        imagesDiodes = diode.GetComponentsInChildren<Image>(true);
+        imagesCL = imageCL.GetComponentsInChildren<Image>(true);
+
+
+        listOfTextCL = new TextMeshProUGUI[imagesCL.Length];
+        for (int i = 0; i < imagesCL.Length; i++)
+            listOfTextCL[i] = imagesCL[i].GetComponentInChildren<TextMeshProUGUI>(true);
+
+
+        // Activate the buttons according to the number of CLs to collect
+        for (int i = 0; i < imagesCL.Length; i++)
+            imagesCL[i].gameObject.SetActive(false);
+
+        for (int i = 0; i < imagesDiodes.Length; i++)
+            imagesDiodes[i].sprite = diodeOFF;
+    }
+
+    public void Recolt ()
+    {
+        if (SC_GM_Local.gm.numberOfCLRecover == 1)
+            PlayRecolt(0);
+        else if (SC_GM_Local.gm.numberOfCLRecover == 2)
+            PlayRecolt(1);
+        else if (SC_GM_Local.gm.numberOfCLRecover == 3)
+        {
+            PlayRecolt(2);
+            goToPreparatoryPhase.interactable = true;
+        }
+    }
+
+    private void PlayRecolt(int nbCL)
+    {
+        imagesDiodes[nbCL].sprite = diodeON;
+        listOfTextCL[nbCL].text = SC_GM_Local.gm.wordsInCollect[nbCL].word[0].titre;
+        imagesCL[nbCL].gameObject.SetActive(true);
+
+    }
+}
