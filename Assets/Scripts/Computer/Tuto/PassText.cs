2f6f9415e5e768645f4d65f2d6222fec02fe6297
Virgile <virgilepauli@hotmail.fr>
2020-02-24 14:40:32 +0100

    modifs anim stampy/radio + début tuto

---
+++ Assets/Scripts/Computer/Tuto/PassText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassText : MonoBehaviour
+{
+    public string[] TextTuto;
+    public Text ActualText;
+    private int NumberText;
+    
+    
+    void Start()
+    {
+        NumberText = 0;
+        
+    }
+
+    public void NextText()
+    {
+        if (TextTuto[NumberText] != null)
+        {
+            ActualText.GetComponent<Text>().text = TextTuto[NumberText];
+            NumberText++;
+        }
+        else
+        {
+
+        }
+        
+    }
+}
