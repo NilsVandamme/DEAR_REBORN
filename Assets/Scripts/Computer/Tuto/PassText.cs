77d3ecbf485945a231f86dd116a5618464f066b3
Virgile <virgilepauli@hotmail.fr>
2020-02-24 18:32:36 +0100

    avancée tuto + tweak anim

--- Assets/Scripts/Computer/Tuto/PassText.cs
+++ Assets/Scripts/Computer/Tuto/PassText.cs
@@ -8,25 +8,58 @@ public class PassText : MonoBehaviour
     public string[] TextTuto;
     public Text ActualText;
     private int NumberText;
+    public GameObject PrevButton;
+    public GameObject NextButton;
+    public GameObject Stampy;
     
     
     void Start()
     {
         NumberText = 0;
-        
+        PrevButton.SetActive(false);
     }
 
     public void NextText()
     {
-        if (TextTuto[NumberText] != null)
+        NumberText++;
+        PrevButton.SetActive(true);
+        if (TextTuto[NumberText] != "")
         {
             ActualText.GetComponent<Text>().text = TextTuto[NumberText];
-            NumberText++;
         }
         else
         {
-
+            Stampy.GetComponent<StampyNext>().PlayAnim();
         }
         
+    }
+
+    public void PrevText()
+    {
+        NumberText--;
+        if (NumberText >= 0)
+        {
+            ActualText.GetComponent<Text>().text = TextTuto[NumberText];
+        }
+        if (NumberText == 0)
+        {
+            PrevButton.SetActive(false);
+        }
+        if (NumberText <= 0)
+        {
+            PrevButton.SetActive(false);
+            NumberText = 0;
+        }
+
+    }
+
+    public void GenerateText()
+    {
+        ActualText.GetComponent<Text>().text = TextTuto[NumberText];
+    }
+
+    public void Disapear()
+    {
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Computer/Tuto/StampyNext.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampyNext : MonoBehaviour
+{
+    public GameObject[] Bulles;
+    private int NumberBulles;
+    public Animator StampyAnimator;
+
+    void Start()
+    {
+        NumberBulles = 0;
+    }
+
+    public void PlayAnim()
+    {
+        StampyAnimator.Play(NumberBulles.ToString());
+    }
+
+    public void NextPos()
+    {
+        Bulles[NumberBulles-1].GetComponent<Animator>().Play("DisparitionBulle");
+        Bulles[NumberBulles].SetActive(true);
+        NumberBulles++;
+    }
+
+    public void Initialized()
+    {
+        Bulles[NumberBulles].SetActive(true);
+        NumberBulles++;
+    }
+
+}
