0d5c6072929ddd7a2049d90efbaa696c03c1b4ed
Virgile <virgilepauli@hotmail.fr>
2020-03-23 16:51:34 +0100

    debut stampy global

---
+++ Assets/Scripts/Computer/Tuto/StampyManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampyManager : MonoBehaviour
+{
+    private bool FlipFlop;
+    public GameObject Mails;
+    public GameObject Daily;
+    public GameObject Arbo;
+    public GameObject BulleMails;
+    public GameObject BulleDaily;
+    public GameObject BulleArbo;
+    public GameObject Stampy;
+
+    public void StampyButton()
+    {
+        if (!FlipFlop)
+        {
+            FlipFlop = true;
+            SelectBulle();
+            return;
+        }
+        else
+        {
+            FlipFlop = false;
+            Stampy.GetComponent<StampyNext>().LeftForNow();
+            return;
+        }
+    }
+
+    public void SelectBulle()
+    {
+        if (Mails.SetActive == true)
+        {
+            Stampy.GetComponent<StampyNext>().Bulles[0] = BulleMails;
+            Stampy.SetActive = true;
+            return;
+        }
+        if (Daily.SetActive == true)
+        {
+            Stampy.GetComponent<StampyNext>().Bulles[0] = BulleDaily;
+            Stampy.SetActive = true;
+            return;
+        }
+        if (Arbo.SetActive == true)
+        {
+            Stampy.GetComponent<StampyNext>().Bulles[0] = BulleArbo;
+            Stampy.SetActive = true;
+            return;
+        }
+    }
+}
