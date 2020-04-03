f546706af8f4efd33ea6c7cfce2f6409a9953833
Julien <j.crepin@rubika-edu.com>
2020-04-03 15:30:20 +0200

    Archive

--- Assets/Scripts/Computer/SC_InfoPersoForArchive.cs
+++ Assets/Scripts/Computer/SC_InfoPersoForArchive.cs
@@ -9,6 +9,6 @@ public class SC_InfoPersoForArchive : MonoBehaviour
     private void Start()
     {
         SC_GM_Master.gm.descriptionPerso[SC_GM_Local.gm.persoOfCurrentScene] = desc.text;
-        SC_GM_Master.gm.infoPerso[SC_GM_Local.gm.persoOfCurrentScene] = info.text.Replace("\n", " / ").Replace("  ", " ");
+        SC_GM_Master.gm.infoPerso[SC_GM_Local.gm.persoOfCurrentScene] = info.text.Replace(System.Environment.NewLine, " / ");
     }
 }
