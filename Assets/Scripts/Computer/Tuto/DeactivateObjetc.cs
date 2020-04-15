8a9f342aa21dd350effdd8161d565ff51c5e73ad
Virgile <virgilepauli@hotmail.fr>
2020-04-15 16:49:41 +0200

    correction stampy tuto + stampy

---
+++ Assets/Scripts/Computer/Tuto/DeactivateObjetc.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeactivateObjetc : MonoBehaviour
+{
+    public GameObject TheObject;
+
+    public void Deactivate()
+    {
+        Debug.Log("oui");
+        TheObject.GetComponent<Button>().interactable = false;
+    }
+}
