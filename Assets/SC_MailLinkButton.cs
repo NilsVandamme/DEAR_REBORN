274692c844d7d9a83e5b31e7a142ca8626b8a851
Djuloh <j.tien@rubika-edu.com>
2020-06-29 15:30:50 +0200

    Si K. K. o Mode

---
+++ Assets/SC_MailLinkButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MailLinkButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PopUp;
+    void Start()
+    {
+        
+    }
+
+    public void SetActivePopUp()
+    {
+        PopUp.SetActive(true);
+
+    }
+
+    public void ClosePopUp()
+    {
+        PopUp.SetActive(false);
+    }
+    
+   
+    public void TextHover()
+    {
+        
+            SC_GM_Cursor.gm.changeToHoverCursor();
+    }
+
+
+    
+    public void TextHoverExit()
+    {
+        SC_GM_Cursor.gm.changeToNormalCursor();
+    }
+
+
+}
