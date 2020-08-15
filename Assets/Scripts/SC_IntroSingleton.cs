129d2c3c71d49fb2bf1719f7feadcf4a0b1cec13
Nils <nils.vandamme98@gmail.com>
2020-08-15 18:24:31 +0200

    Quelques fix menu

---
+++ Assets/Scripts/SC_IntroSingleton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_IntroSingleton : MonoBehaviour
+{
+    public static SC_IntroSingleton instance;
+
+    private void Awake()
+    {
+        if (SC_IntroSingleton.instance == null)
+            SC_IntroSingleton.instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+}
