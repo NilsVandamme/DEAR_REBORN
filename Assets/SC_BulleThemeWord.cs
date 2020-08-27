a25dc8c92a3c58d6b52e933ca8662b9dd03e486e
Djuloh <j.tien@rubika-edu.com>
2020-08-27 23:43:25 +0200

    les anims du feedback canvas

---
+++ Assets/SC_BulleThemeWord.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_BulleThemeWord : MonoBehaviour
+{
+    public Vector3 LeftPosition, MiddlePosition, RightPosition;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+public void GoToLeft()
+    {
+        anim.Play("BulleLeft");
+    }
+
+    public void GotoMiddle()
+    {
+        anim.Play("BulleMiddle");
+    }
+
+    public void GoToRight()
+    {
+        anim.Play("BulleRight");
+    }
+
+    public void MakeWordAppear()
+    {
+        //pop le mot à sa position
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/SC_FeedbackPlayerScript.cs
+++ Assets/SC_FeedbackPlayerScript.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class SC_FeedbackPlayerScript : MonoBehaviour
 {
     private SC_FeedbackCanvasText fb;
+    public GameObject bulleThemeWord1, bulleThemeWord2, bulleThemeWord3;
+    public int bulleInt;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,27 @@ public class SC_FeedbackPlayerScript : MonoBehaviour
         fb.updateText();
     }
 
+    public void SpawnBulle()
+    {
+        //TO DO : Open Word Basket
+
+        if (bulleInt == 1)
+        {
+            bulleThemeWord1.SetActive(true);
+        }
+      
+
+        if (bulleInt == 2)
+        {
+            bulleThemeWord2.SetActive(true);
+        }
+
+        if (bulleInt == 3)
+        {
+            bulleThemeWord3.SetActive(true);
+        }
+
+    }
  
     // Update is called once per frame
     void Update()
