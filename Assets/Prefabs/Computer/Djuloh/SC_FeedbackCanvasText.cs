26bd4d5a8fc19532d07b6e192fb7c32e33639613
Djuloh <j.tien@rubika-edu.com>
2020-09-03 23:27:36 +0200

    début d'ajout des feedback d'infos

--- Assets/Prefabs/Computer/Djuloh/SC_FeedbackCanvasText.cs
+++ Assets/Prefabs/Computer/Djuloh/SC_FeedbackCanvasText.cs
@@ -14,7 +14,7 @@ public class SC_FeedbackCanvasText : MonoBehaviour
     private string characterName="[CHARA]";
     public static int characterCurrentInt=0;
     public int characterMaxInt=3;
-    public static bool Donna1 = false, Donna2 = false, Donna3 = false, MrS1 = false, MrS2 = false, MrS3 = false, Ash1 = false, Ash2 = false, Ash3 = false, Kat1 = false, Kat2 = false, Kat3 = false, Lyle1 = false, Lyle2 = false, Lyle3 = false , Kosong1 = false, Kosong2 = false, Kosong3 = false;
+    public static bool Cyrus1 = false, Cyrus2 = false, Cyrus3 = false, Donna1 = false, Donna2 = false, Donna3 = false, MrS1 = false, MrS2 = false, MrS3 = false, Ash1 = false, Ash2 = false, Ash3 = false, Kat1 = false, Kat2 = false, Kat3 = false, Lyle1 = false, Lyle2 = false, Lyle3 = false , Kosong1 = false, Kosong2 = false, Kosong3 = false;
     
     // Start is called before the first frame update
     void Start()
@@ -58,128 +58,134 @@ public class SC_FeedbackCanvasText : MonoBehaviour
         Scene scene = SceneManager.GetActiveScene();
         if ((scene.name == "L_A1") && (Donna1 = false))
         {
+            //ok
             Donna1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Donna2 = false))
         {
             Donna2 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Donna3 = false))
         {
             Donna3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Kat1 = false))
         {
             Kat1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Kat2 = false))
         {
             Kat2 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Kat3 = false))
         {
             Kat3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
-        if ((scene.name == "L_A1") && (Lyle1 = false))
+        if ((scene.name == "L_D1") && (Lyle1 = false))
         {
+            //ok
             Lyle1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Lyle2 = false))
         {
             Lyle2 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Lyle3 = false))
         {
             Lyle3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Ash1 = false))
         {
             Ash1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Ash2 = false))
         {
             Ash2 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (Ash3 = false))
         {
             Ash3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
-        if ((scene.name == "L_A1") && (MrS1 = false))
+        if (((scene.name == "L_B1")||(scene.name=="L_B2")) && (MrS1 = false))
         {
+            //ok
             MrS1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (MrS2 = false))
         {
             MrS2 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
         if ((scene.name == "L_A1") && (MrS3 = false))
         {
             MrS3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
-        if ((scene.name == "L_A1") && (Kosong1 = false))
+        if ((scene.name == "L_F2") && (Kosong1 = false))
         {
+            //ok
             Kosong1 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
             return;
         }
-        if ((scene.name == "L_A1") && (Kosong2 = false))
+        if ((scene.name == "L_F3") && (Kosong2 = false))
         {
+            //ok
             Kosong2 = true;
             characterMaxInt = 3;
-            characterCurrentInt++;
             return;
         }
         if ((scene.name == "L_A1") && (Kosong3 = false))
         {
             Kosong3 = true;
             characterCurrentInt++;
-            characterMaxInt = 3;
+            return;
+        }
+        if ((scene.name == "L_F4") && (Cyrus1 = false))
+        {
+            //ok
+            Cyrus1 = true;
+            characterCurrentInt++;
+            return;
+        }
+        if ((scene.name == "L_A1") && (Cyrus2 = false))
+        {
+            Cyrus2 = true;
+            characterCurrentInt++;
+            return;
+        }
+        if ((scene.name == "L_A1") && (Cyrus3 = false))
+        {
+            Cyrus3= true;
+            characterCurrentInt++;
             return;
         }
         characterName = charName;
